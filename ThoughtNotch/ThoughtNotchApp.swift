23d9fa2bd9eeab79fb8e8b02b0b41f5b0c09c6d8
Louis Holdsworth <louisholdsworth@gmail.com>
2026-04-27 10:19:42 +0100

    Initial Commit

---
+++ ThoughtNotch/ThoughtNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThoughtNotchApp.swift
+//  ThoughtNotch
+//
+//  Created by Louis Holdsworth on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ThoughtNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
